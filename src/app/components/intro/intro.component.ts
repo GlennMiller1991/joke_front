7ba01ecccd14d1accd89457b373c385efd8e6973
AlexB <gatesoftommorow91@mail.ru>
2024-09-28 02:38:29 +0400

    intro component

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, OnDestroy, ViewChild, afterNextRender, afterRender } from "@angular/core";
+import { log } from "console";
+
 
 @Component({
     standalone: true,
@@ -6,6 +8,41 @@ import { Component } from "@angular/core";
     templateUrl: './intro.component.html',
     styleUrl: './intro.component.css'
 })
-export class IntroComponent {
-    
+export class IntroComponent implements OnDestroy {
+    @ViewChild('canvas')
+    canvasRef!: ElementRef<HTMLCanvasElement>
+    resizeObserver!: ResizeObserver
+    parent!: HTMLElement
+    canvas!: HTMLCanvasElement
+    ctx!: WebGL2RenderingContext
+
+    constructor() {
+        afterNextRender(() => {
+            this.canvas = this.canvasRef.nativeElement
+            this.parent = this.canvas.parentElement!
+            this.canvas.style.background = 'inherit'
+            if (!this.parent) return
+            this.ctx = this.canvas.getContext('webgl2')!
+            if (!this.ctx) return
+            this.resizeObserver = new ResizeObserver(this.onResize)
+            this.resizeObserver.observe(this.canvas.parentElement!)
+        })
+    }
+
+    get isReady() {
+        return this.canvas && this.parent && this.ctx
+    }
+
+    onResize = () => {
+        const parent = this.canvasRef.nativeElement.parentElement!
+        const rect = parent.getBoundingClientRect()
+        this.canvasRef.nativeElement.width = rect.width
+        this.canvasRef.nativeElement.height = rect.height
+    }
+
+    ngOnDestroy() {
+        this.resizeObserver?.disconnect
+        this.resizeObserver = null as any
+    }
+
 }
