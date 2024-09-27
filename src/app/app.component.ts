bff50c203beac1179cf895aa278f89bcf0432f9f
AlexB <gatesoftommorow91@mail.ru>
2024-09-27 12:32:21 +0400

    some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from './components/intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'Fat Bass Ltd.';
+
 }
---
+++ src/app/components/intro/intro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'intro-component',
+    templateUrl: './intro.component.html',
+    styleUrl: './intro.component.css'
+})
+export class IntroComponent {
+    
+}
