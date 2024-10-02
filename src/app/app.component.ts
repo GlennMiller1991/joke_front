62ede4fe752c85ebe84d6594078c89694c8717da
AlexB <gatesoftommorow91@mail.ru>
2024-10-02 07:45:53 +0400

    preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IntroComponent } from './components/intro/intro.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IntroComponent],
+  imports: [RouterOutlet, IntroComponent, PreloaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/preloader/preloader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'preloader-component',
+    templateUrl: './preloader.component.html',
+    styleUrl: './preloader.component.css'
+})
+export class PreloaderComponent {
+    
+}
