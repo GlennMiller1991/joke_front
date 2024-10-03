079f907948cd8508653c05f2a1c89bda3fed32d4
AlexB <gatesoftommorow91@mail.ru>
2024-10-03 09:48:53 +0400

    button

---
+++ src/app/components/preloader/components/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'button-component',
+    templateUrl: './button.component.html',
+    styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
--- src/app/components/preloader/preloader.component.ts
+++ src/app/components/preloader/preloader.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
+import { ButtonComponent } from "./components/button.component";
 
 @Component({
     standalone: true,
     selector: 'preloader-component',
     templateUrl: './preloader.component.html',
-    styleUrl: './preloader.component.css'
+    styleUrl: './preloader.component.css',
+    imports: [
+        ButtonComponent
+    ]
 })
 export class PreloaderComponent {
     
