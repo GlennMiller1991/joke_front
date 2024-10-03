be501410ba7584e61a61c1da5f727499d1f58c6b
AlexB <gatesoftommorow91@mail.ru>
2024-10-03 09:58:04 +0400

    button ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { PreloaderComponent } from './components/preloader/preloader.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IntroComponent],
+  imports: [RouterOutlet, IntroComponent, PreloaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
