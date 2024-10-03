9115dd4c6b336f06d249a4944bc63ef305636474
AlexB <gatesoftommorow91@mail.ru>
2024-10-03 10:41:12 +0400

    button props

--- src/app/components/preloader/components/button.component.ts
+++ src/app/components/preloader/components/button.component.ts
@@ -1,11 +1,30 @@
-import { Component } from "@angular/core";
+import { NgClass, NgStyle } from "@angular/common";
+import { Component, Input } from "@angular/core";
 
 @Component({
     standalone: true,
     selector: 'button-component',
     templateUrl: './button.component.html',
-    styleUrl: './button.component.css'
+    styleUrl: './button.component.css',
+    imports: [
+        NgClass, NgStyle,
+    ]
 })
 export class ButtonComponent {
+    @Input() baseType: 'flush' | 'elevated' = 'elevated'
+    @Input() size: string = '7em'
 
+    get classes() {
+        return {
+            btn: true,
+            elevated: this.baseType === 'elevated'
+        }
+    }
+
+    get btnStyles() {
+        return {
+            height: this.size,
+            width: this.size,
+        }
+    }
 }
